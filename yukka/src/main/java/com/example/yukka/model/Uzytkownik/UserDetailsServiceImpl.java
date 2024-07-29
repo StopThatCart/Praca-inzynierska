5134f305e55b15dee2cec0f114a7b9f9f388aaab
Łukasz Domin <ld123631@stud.ur.edu.pl>
2024-07-29 22:19:05 +0200

    Teraz działa i tak na razie zostawię

--- yukka/src/main/java/com/example/yukka/model/Uzytkownik/UserDetailsServiceImpl.java
+++ yukka/src/main/java/com/example/yukka/model/Uzytkownik/UserDetailsServiceImpl.java
@@ -1,12 +1,9 @@
 package com.example.yukka.model.Uzytkownik;
 
 import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -28,26 +25,11 @@ public class UserDetailsServiceImpl implements  UserDetailsService {
                 .orElseThrow(() -> new UsernameNotFoundException("Użytkownika nie znaleziono"));
     }
 
-   
-    public UserDetails loadUserByUsername2(String username) throws UsernameNotFoundException {
-        Uzytkownik uzytkownik = repository.findByEmail(username)
-            .orElseThrow(() -> new UsernameNotFoundException("User not found"));
-
-        Set<GrantedAuthority> authorities = new HashSet<>();
-        if (uzytkownik.isAdmin()) {
-            authorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
-        }
-        if (uzytkownik.isPracownik()) {
-            authorities.add(new SimpleGrantedAuthority("ROLE_PRACOWNIK"));
-        }
-
-        return new org.springframework.security.core.userdetails.User(
-            uzytkownik.getUsername(),
-            uzytkownik.getPassword(),
-            authorities
-        );
-    }
     public Collection<Uzytkownik> getAllUsers(){
         return repository.getAllUsers();
+    }
+
+    public Optional<Uzytkownik> dawajEmailDeklu(String userEmail){
+        return repository.findByEmail(userEmail);
     }
 }
--- yukka/src/main/java/com/example/yukka/security/SecurityConfig.java
+++ yukka/src/main/java/com/example/yukka/security/SecurityConfig.java
@@ -6,82 +6,30 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 import lombok.RequiredArgsConstructor;
 
+
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 @RequiredArgsConstructor
-@EnableMethodSecurity(securedEnabled=false)
 public class SecurityConfig {
 
-    private JwtFilter jwtAuthFilter;
     private final AuthenticationProvider authenticationProvider;
 
-// Configuring HttpSecurity
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.csrf((CsrfConfigurer<HttpSecurity> csrf) -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth.requestMatchers(
-                                                            "/rest/neo4j/**",
-                                                            "/auth/welcome", 
-                                                            "/auth/register", 
-                                                            "/auth/users", 
-                                                            "/auth/generateToken")
-                                                            .permitAll())
-               // .authorizeHttpRequests(auth -> auth.requestMatchers("/auth/user/**").authenticated())
-                .authorizeHttpRequests(auth -> auth.requestMatchers("/auth/admin/**").authenticated())
+        return http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth.requestMatchers( "/**").permitAll())
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider)
-              //  .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+              //  .addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
-    /*
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-            .cors(Customizer.withDefaults())
-            .csrf(AbstractHttpConfigurer::disable)
-            .authorizeHttpRequests(req ->
-                        req.requestMatchers(
-                            "/auth/**",
-                            // Reszta do swaggera
-                            "/v2/api-docs",
-                            "/configuration/ui",
-                            "/swagger-resources/**",
-                            "/configuration/security",
-                            "/swagger-ui.html",
-                            "/webjars/**"
-                        ).permitAll()
-                                    .anyRequest()
-                                        .authenticated()
-                     )
-            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .authenticationProvider(authenticationProvider)
-            //.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
-            ;
-        return http.build();
-    }
-    */
+    // Password Encoding
 
-
-    /* 
-    @Autowired
-    private Neo4jAuthenticationProvider authenticationProvider;
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(authenticationProvider);
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().authenticated()
-                .and().httpBasic();
-    }
-*/
 }
