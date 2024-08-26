b0c52c88a13caedb9da04fe21f298477a9183159
Łukasz Domin <ld123631@stud.ur.edu.pl>
2024-08-26 14:11:47 +0200

    Zapomniałem statusów lol

--- yukka/src/main/java/com/example/yukka/model/dzialka/controller/DzialkaController.java
+++ yukka/src/main/java/com/example/yukka/model/dzialka/controller/DzialkaController.java
@@ -1,9 +1,10 @@
 package com.example.yukka.model.dzialka.controller;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -37,27 +38,25 @@ public class DzialkaController {
 
 
     @GetMapping
-    public List<Dzialka> getDzialki(Authentication connectedUser) {
-        return dzialkaService.getDzialki(connectedUser);
+    public ResponseEntity<List<Dzialka>> getDzialki(Authentication connectedUser) {
+        return ResponseEntity.ok(dzialkaService.getDzialki(connectedUser));
     }
 
     @GetMapping("/uzytkownicy/{nazwa}")
-    public List<Dzialka> getDzialkiOfUzytkownik(@PathVariable String nazwa, Authentication connectedUser) {
-        return dzialkaService.getDzialkiOfUzytkownik(nazwa, connectedUser);
+    public ResponseEntity<List<Dzialka>> getDzialkiOfUzytkownik(@PathVariable String nazwa, Authentication connectedUser) {
+        return ResponseEntity.ok(dzialkaService.getDzialkiOfUzytkownik(nazwa, connectedUser));
     }
 
     @GetMapping("/{numer}")
-    public Optional<Dzialka> getDzialkaByNumer(@PathVariable int numer, Authentication connectedUser) {
-        return dzialkaService.getDzialkaByNumer(numer, connectedUser);
+    public ResponseEntity<Dzialka> getDzialkaByNumer(@PathVariable int numer, Authentication connectedUser) {
+        return ResponseEntity.ok(dzialkaService.getDzialkaByNumer(numer, connectedUser).orElse(null));
     }
 
     @GetMapping("/{numer}/uzytkownicy/{nazwa}")
-    public Optional<Dzialka> getDzialkaOfUzytkownikByNumer(@PathVariable int numer, 
+    public ResponseEntity<Dzialka> getDzialkaOfUzytkownikByNumer(@PathVariable int numer, 
     @PathVariable String nazwa) {
-        return dzialkaService.getDzialkaOfUzytkownikByNumer(numer, nazwa);
+        return ResponseEntity.ok(dzialkaService.getDzialkaOfUzytkownikByNumer(numer, nazwa).orElse(null));
     }
-
-    
 
     /* 
     @DeleteMapping("/{id}")
@@ -66,24 +65,26 @@ public class DzialkaController {
     }
 */
     @PostMapping("/rosliny")
-    public Dzialka saveRoslinaToDzialka(@Valid @RequestBody DzialkaRoslinaRequest request, Authentication connectedUser) {
-        return dzialkaService.saveRoslinaToDzialka(request, connectedUser);
+    public ResponseEntity<Dzialka> saveRoslinaToDzialka(@Valid @RequestBody DzialkaRoslinaRequest request, Authentication connectedUser) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(dzialkaService.saveRoslinaToDzialka(request, connectedUser));
     }
 
     @PatchMapping(value = "/rosliny/obraz", consumes = "multipart/form-data")
-    public Dzialka updateRoslinaObrazInDzialka(@Valid @RequestBody DzialkaRoslinaRequest request,
+    public ResponseEntity<Dzialka> updateRoslinaObrazInDzialka(@Valid @RequestBody DzialkaRoslinaRequest request,
         @Parameter() @RequestPart("file") MultipartFile file,
         Authentication connectedUser) {
-        return dzialkaService.updateRoslinaObrazInDzialka(request, file, connectedUser);
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(dzialkaService.updateRoslinaObrazInDzialka(request, file, connectedUser));
     }
 
     @DeleteMapping("/rosliny")
-    public void deleteRoslinaFromDzialka(@Valid @RequestBody DzialkaRoslinaRequest request, Authentication connectedUser) {
+    public ResponseEntity<String> deleteRoslinaFromDzialka(@Valid @RequestBody DzialkaRoslinaRequest request, Authentication connectedUser) {
         dzialkaService.deleteRoslinaFromDzialka(request, connectedUser);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
     @DeleteMapping("/rosliny/obraz")
-    public void deleteRoslinaObrazFromDzialka(@Valid @RequestBody DzialkaRoslinaRequest request, Authentication connectedUser) {
+    public ResponseEntity<String> deleteRoslinaObrazFromDzialka(@Valid @RequestBody DzialkaRoslinaRequest request, Authentication connectedUser) {
         dzialkaService.deleteRoslinaObrazInDzialka(request, connectedUser);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
