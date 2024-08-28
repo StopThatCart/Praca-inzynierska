a6bcad1d75920ce0871ea9012605baae1682cc91
Łukasz Domin <ld123631@stud.ur.edu.pl>
2024-08-28 13:39:05 +0200

    Dodanie konfiguracji dokumentacji OpenApi

---
+++ yukka/src/main/java/com/example/yukka/security/config/OpenApiConfig.java
@@ -0,0 +1,57 @@
+package com.example.yukka.security.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "Łukasz Domin",
+                        email = "lukaszdomin@interia.pl"
+                ),
+                description = "Dokumentacja OpenApi dla Spring Security",
+                title = "Spring Security OpenApi",
+                version = "0.3",
+                license = @License(
+                        name = "Licence name",
+                        url = "https://some-url.com"
+                ),
+                termsOfService = "Terms of service"
+        ),
+        servers = {
+                @Server(
+                        description = "Local ENV",
+                        url = "http://localhost:8081/rest/neo4j"
+                ),
+                @Server(
+                        description = "Godot ENV",
+                        url = "http://localhost:8082"
+                ),
+                @Server(
+                        description = "PROD ENV",
+                        url = "https://aliboucoding.com/course"
+                )
+        },
+        security = {
+                @SecurityRequirement(
+                        name = "bearerAuth"
+                )
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "Authorization Bearer JWT",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig {
+}
