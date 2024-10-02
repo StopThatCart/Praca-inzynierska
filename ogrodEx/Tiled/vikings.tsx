ca0903ae4bad8c6d6c5f077c725df962005ce3ac
Łukasz Domin <ld123631@stud.ur.edu.pl>
2024-10-02 16:11:40 +0200

    Usunięcie niepotrzebnych elementów

--- ogrodEx/Tiled/vikings.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="Vikings_tilemaps_016" tilewidth="20" tileheight="20" tilecount="160" columns="16">
- <image source="Vikings_tilemaps_016.bmp" trans="ff00ff" width="320" height="200"/>
-</tileset>
